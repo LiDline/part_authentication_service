56121371a49bccfe05971d813096f8bd1c7ba540
LiDline <artyom.zhevnerenko@yandex.ru>
2024-08-29 12:59:01 +0300

    DB. Добавлена PostgreeSQL в проект

---
+++ init.sql
@@ -0,0 +1,19 @@
+﻿DROP TABLE IF EXISTS refresh_tokens;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  guuid UUID PRIMARY KEY,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE refresh_tokens (
+  refresh_token TEXT PRIMARY KEY,
+  created_at TIMESTAMP,
+  guuid UUID NOT NULL,
+  FOREIGN KEY (guuid)  REFERENCES users (guuid) ON DELETE CASCADE
+);
+
+INSERT INTO users (guuid, password)
+VALUES ('57979158-bc47-490c-87fb-183d9b7a99d4', 'YWRtaW4=');
+
+
